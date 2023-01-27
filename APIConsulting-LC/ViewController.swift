9bc36570a9d2a4db2e901ec4a0c9b5fc7bc4d488
Luciano Castro <lfcastro2911@gmail.com>
2023-01-27 09:59:19 -0300

    Initial commit

---
+++ APIConsulting-LC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIConsulting-LC
+//
+//  Created by u633168 on 27/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
